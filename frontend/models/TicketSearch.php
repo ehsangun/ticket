b3cbfb207caa747a903c0191551af7e001d30239
kasrarsh <kasramagers1377@gmail.com>
2019-06-29 21:06:30 +0430

    solve the namespace of ticket model,ticket controller,views and genarate them again

---
+++ frontend/models/TicketSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace frontend\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Ticket;
+
+/**
+ * TicketSearch represents the model behind the search form of `frontend\models\Ticket`.
+ */
+class TicketSearch extends Ticket
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['ID', 'IdCustomer', 'IdAdmin'], 'integer'],
+            [['subject', 'products', 'description', 'answer', 'created_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Ticket::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'ID' => $this->ID,
+            'IdCustomer' => $this->IdCustomer,
+            'IdAdmin' => $this->IdAdmin,
+            'created_at' => $this->created_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'subject', $this->subject])
+            ->andFilterWhere(['like', 'products', $this->products])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'answer', $this->answer]);
+
+        return $dataProvider;
+    }
+}
