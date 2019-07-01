7bd74c02f6b8850cf500f7232e631fb8b976dc56
ehsangun <matari.ehsan@yahoo.com>
2019-07-01 09:44:49 +0430

    text input to rextarea

--- frontend/views/ticket/_form.php
+++ frontend/views/ticket/_form.php
@@ -22,7 +22,7 @@ use yii\widgets\ActiveForm;
 
     ) ?>
 
-    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'description')->textarea(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
