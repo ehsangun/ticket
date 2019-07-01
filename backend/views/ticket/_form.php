e566b359daa57e36b738d3848fd149a63ab99f4a
ehsangun <matari.ehsan@yahoo.com>
2019-07-01 09:45:04 +0430

    text input to rextarea

---
+++ backend/views/ticket/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Ticket */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'subject')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'products')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'IdCustomer')->textInput() ?>
+
+    <?= $form->field($model, 'IdAdmin')->textInput() ?>
+
+    <?= $form->field($model, 'created_at')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/ticket/_search.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\TicketSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ticket-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'ID') ?>
+
+    <?= $form->field($model, 'subject') ?>
+
+    <?= $form->field($model, 'products') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?= $form->field($model, 'answer') ?>
+
+    <?php // echo $form->field($model, 'IdCustomer') ?>
+
+    <?php // echo $form->field($model, 'IdAdmin') ?>
+
+    <?php // echo $form->field($model, 'created_at') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
