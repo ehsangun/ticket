63601764d81743c4389c2b875e64a14855003a57
ehsangun <matari.ehsan@yahoo.com>
2019-07-14 13:57:58 +0430

    index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,7 +3,8 @@
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
-?>
+
+use yii\helpers\Html; ?>
 <div class="site-index">
 
     <div class="jumbotron">
@@ -18,7 +19,7 @@ $this->title = 'My Yii Application';
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>ارسال تیکت</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -28,7 +29,7 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>تیکت ها</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -38,14 +39,27 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2><?php
+                    if (Yii::$app->user->isGuest){
+                        echo 'ورود';
+                    }else if (!Yii::$app->user->isGuest){
+                        echo 'خروج';
+                    }
+
+                    ?></h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur.</p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><?php
+                    if (Yii::$app->user->isGuest){
+                        echo Html::a('Login', ['login'], ['class' => 'btn btn-default']);
+                    }else if (!Yii::$app->user->isGuest){
+                        echo Html::a('Logout', ['logout'], ['class' => 'btn btn-default']);
+                    }
+                    ?></p>
             </div>
         </div>
 
